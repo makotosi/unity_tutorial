a5690a6943fccaab687fb1c5c7ecd2ac92e876b4
makotosiraisi <makotosiraisi3@gmail.com>
2016-10-15 14:52:58 +0900

    first commit

---
+++ BollBoxScript.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var ball : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+    if(Input.GetButtonUp("Jump")){
+        Instantiate(ball, transform.position, transform.rotation);
+    }
+}
---
+++ PlayerScript.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var x : int = 7;
+
+function Start () {
+    Debug.Log("Hello World ->" + x);
+}
+
+function Update () {
+    if(Input.GetButtonUp("Jump")){
+        Debug.Log("Jumped");
+    }
+    var x : float = Input.GetAxis("Horizontal");
+    transform.Translate(x * 0.2, 0, 0);
+    //transform.position.z += 0.1;
+    //transform.position += Vector3(0, 0, 0.1);
+    //transform.Translate(0, 0, 0.1);
+}
+
+function OnCollisionEnter(obj : Collision){
+    if(obj.gameObject.name == "rightwall"){
+        Debug.Log("Hit Right Wall");
+    }
+}
